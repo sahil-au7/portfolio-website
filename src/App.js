efb53dd4d26217445f15af78d5f7537c1f488dc8
sahil-au7 <sahilyadav601@gmail.com>
2021-01-31 22:58:39 +0530

    setup done

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Header from "./UI/Header";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>hello</h1>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
